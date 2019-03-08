60eafb688f9bf73943c2efefcddae88c9423dab1
David Thai <davidthai@dafuloth.me>
2019-03-08 14:54:33 +0000

    TDD Thermostat.up(), test passing

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,7 +1,23 @@
-describe('thermostat starts at 20 degrees', function() {
-  it('starts at 20 degrees', function () {
+describe('Thermostat Features', () => {
+  
+  describe('thermostat', function() {
     var thermostat
-    thermostat = new Thermostat
-    expect(thermostat.temperature).toEqual (20);
+
+    beforeEach(() => {
+      thermostat = new Thermostat
+    });
+
+    it('starts at 20 degrees', function () {
+      expect(thermostat.temperature).toEqual (20);
+    });
+
+    it('increases temperature with an up function', () => {
+      var initialTemp = thermostat.temperature
+      var tempChange = 2;
+      thermostat.up(tempChange);
+      expect(thermostat.temperature).toEqual(initialTemp+tempChange)
+    });
+
   });
+
 });
