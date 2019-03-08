df7f7ca65eb9ea97ad2ad0647f28e5b93f262f0a
Amina Jusic <amina_jusic@yahoo.com>
2019-03-08 15:17:43 +0000

    Passed the test for the decrease the temp

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,23 +1,33 @@
 describe('Thermostat Features', () => {
-  
-  describe('thermostat', function() {
-    var thermostat
 
-    beforeEach(() => {
-      thermostat = new Thermostat
-    });
+  // describe('thermostat', function() {
+  //   var thermostat;
+
+
+
 
     it('starts at 20 degrees', function () {
+      var thermostat = new Thermostat();
       expect(thermostat.temperature).toEqual (20);
     });
 
     it('increases temperature with an up function', () => {
+      var thermostat = new Thermostat();
       var initialTemp = thermostat.temperature
       var tempChange = 2;
       thermostat.up(tempChange);
-      expect(thermostat.temperature).toEqual(initialTemp+tempChange)
-    });
 
-  });
+      expect(thermostat.temperature).toEqual(initialTemp+tempChange)
+     });
+
+    it('decrease temperature with a down function', function() {
+      var thermostat = new Thermostat();
+      var initialTemp = thermostat.temperature
+      var tempChange = 2;
+      thermostat.down(tempChange);
+      expect(thermostat.temperature).toEqual(initialTemp-tempChange)
+     });
+
+  // });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,7 +1,11 @@
 function Thermostat() {
   this.temperature = 20;
-}
+};
 
 Thermostat.prototype.up = function(number) {
   this.temperature += 2;
+};
+
+Thermostat.prototype.down = function(tempChange) {
+  this.temperature -= 2;
 };
